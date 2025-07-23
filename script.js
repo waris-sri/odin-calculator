ae502378da3255d9b91dda19750b9ba9dd9483e8
Waris Sripatoomrak <167295008+waris-sri@users.noreply.github.com>
2025-07-23 18:59:07 +0700

    refactor: created  to use in both button and key handlers

--- script.js
+++ script.js
@@ -12,15 +12,9 @@ window.addEventListener('DOMContentLoaded', () => {
   note.innerText = nsbp // non-breaking space
 })
 
-// TODO add keyboard support (AC is ESC)
-
 let hasPoint = false
 let isFirstCalculation = false
-
-buttons.addEventListener('click', (e) => {
-  let btn = e.target
-  let type = btn.className
-  let btnVal = btn.innerText
+function handleInput(type, btnVal) {
   switch (type) {
     case 'number':
       if (isFirstCalculation) {
@@ -90,6 +84,13 @@ buttons.addEventListener('click', (e) => {
       if (!result.innerText.includes('.')) hasPoint = false
       break
   }
+}
+
+buttons.addEventListener('click', (e) => {
+  let btn = e.target
+  let type = btn.className
+  let btnVal = btn.innerText
+  handleInput(type, btnVal)
 })
 
 document.addEventListener('keydown', (e) => {
@@ -120,75 +121,7 @@ document.addEventListener('keydown', (e) => {
     return
   }
   e.preventDefault()
-  switch (type) {
-    case 'number':
-      if (isFirstCalculation) {
-        clear()
-        hasPoint = false
-        isFirstCalculation = false
-        result.innerText = '0'
-        note.innerText = nsbp
-      }
-      if (btnVal === '±') {
-        result.innerText *= -1
-      } else {
-        if (result.innerText === '0') result.innerText = btnVal
-        else result.innerText += btnVal
-      }
-      break
-    case 'operator':
-      isFirstCalculation = false // this fixes the entire operation flow bugs
-      hasPoint = false
-      // continuous operations without pressing equal button
-      if (a !== 0 && operator) {
-        b = +result.innerText
-        result.innerText = calculate(a, b, operator)
-        a = +result.innerText
-      } else {
-        a = +result.innerText
-      }
-      operator = btnVal
-      note.innerText = `${a}${btnVal}`
-      result.innerText = '0'
-      break
-    case 'decimal':
-      if (!hasPoint) {
-        result.innerText += btnVal
-        hasPoint = true
-      }
-      break
-    case 'equal':
-      if (!isFirstCalculation) {
-        b = +result.innerText
-        if (operator !== '') {
-          note.innerText = `${a}${operator}${b}`
-        } else {
-          note.innerText = `${b}`
-        }
-      } else {
-        a = +result.innerText // as `b` should stay the same
-        if (operator !== '') {
-          note.innerText = `${a}${operator}${b}`
-        } else {
-          note.innerText = `${a}`
-        }
-      }
-      result.innerText = calculate(a, b, operator)
-      hasPoint = result.innerText.includes('.')
-      isFirstCalculation = true
-      break
-    case 'clear-all':
-      clear()
-      result.innerText = '0'
-      hasPoint = false
-      isFirstCalculation = false
-      break
-    case 'backspace':
-      result.innerText = result.innerText.slice(0, -1)
-      if (result.innerText === '') result.innerText = '0'
-      if (!result.innerText.includes('.')) hasPoint = false
-      break
-  }
+  handleInput(type, btnVal)
 })
 
 function calculate(a, b, operator) {
